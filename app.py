c8ec18d3510fde09c412af4e6feec6bf0467f3f3
igordrsilva <igdouglas.silva@gmail.com>
2024-02-15 18:36:40 -0300

    import os

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, 
@@ -12,8 +13,8 @@ CORS(app,
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:masterkey@127.0.0.1/postgres'
-app.config['SECRET_KEY'] = '659_!si#47sjqc2*r8e2lt6t1u^co^7v1e+pknxy4tim1mu=@c'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://postgres:masterkey@127.0.0.1/postgres')
+app.config['SECRET_KEY'] = os.environ.get('659_!si#47sjqc2*r8e2lt6t1u^co^7v1e+pknxy4tim1mu=@c')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
